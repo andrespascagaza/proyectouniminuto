73889dd2233e5c35ecd3aa0675725f7aa6151ad6
andrespascagaza <116567466+andrespascagaza@users.noreply.github.com>
2022-10-24 08:15:33 -0500

    Add files via upload

---
+++ proyecto/app/src/main/java/com/example/proyecto/INTRO.java
@@ -0,0 +1,14 @@
+package com.example.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class INTRO extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro);
+    }
+}
